028d2d8cd3837583fe6c17edb61ec356904cc934
Laney Smith <lsmith.unc@gmail.com>
2020-04-26 17:54:48 -0700

    add error banner

--- src/components/Header/__tests__/Header.js
+++ src/components/Header/__tests__/Header.js
@@ -5,9 +5,17 @@ import Header from '../Header';
 import { Variable } from '../../../types';
 
 describe('Header', () => {
-  it('renders the correct date and variable', () => {
+  it('renders correctly without error banner', () => {
     const { container } = render(
       <Header selectedVariable={Variable.DEATHS} selectedDate="2020-02-14" />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders correctly with error banner', () => {
+    const { container } = render(
+      <Header selectedVariable={Variable.DEATHS} selectedDate={null} error />
     );
 
     expect(container.firstChild).toMatchSnapshot();
